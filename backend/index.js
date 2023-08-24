90a78038f946468117372e5721e3556a171923f5
Fig-Joaquin <joaquin.sanchez2101@alumnos.ubiobio.cl>
2023-08-24 17:38:01 -0400

    Instalacion de nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,9 @@
 import express from 'express';	// importa express
 
+const app = express();	// Llamado a la función de express
+
+app.use('/', (req, res) => {
+    res.send('App use running');
+})	// Ruta principal
+
+app.listen(4000, () => console.log('Conexión realizada en el puerto 4000'));    // Inicializa el servidor en el puerto 4000
