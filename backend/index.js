f2cf1e2d4f61db8dda927ba40396c2dd6a34ac89
Fig-Joaquin <joaquin.sanchez2101@alumnos.ubiobio.cl>
2023-10-07 21:16:53 -0300

    Backend conectado a la base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from 'express';	// importa express
+import conectarDB from './config/db.js';	// importa la función de conexión a la base de datos
+import dotenv from 'dotenv';	// importa dotenv
 
 const app = express();	// Llamado a la función de express
+dotenv.config();    // Llamado a la función de dotenv
+conectarDB();	// Llamado a la función de conexión a la base de datos
+
 
 app.use('/', (req, res) => {
     res.send('App use running');
