db5b6cd064c3cdbf2a1691eeb2823603045a2ac4
joaquin <joaquinignacio2000@hotmail.com>
2023-12-17 14:44:23 -0300

    banner actualizado

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
-import backgroundImage from './images/LogoConcepcion.png'; // Asegúrate de que la ruta sea correcta
-import {Box, Heading} from '@chakra-ui/react';
+import backgroundImage from './images/LogoConcepcion.png';
+import { Box, Heading } from '@chakra-ui/react';
 
 function App() {
   return (
-      <Box  
+    <Box  
       h='10vh'
       bg='gray'
       bgImage={`url(${backgroundImage})`}
-      bgRepeat={'no-repeat'}
-      bgSize={'cover'}
-      
-      >
-        <Heading
-        color='black'
-        textTransform='uppercase'
-        textAlign='center'
-        fontSize='2xl'
-        fontWeight='bold'
+      bgRepeat='no-repeat'
+      bgSize='cover'
+      position="relative"
+      overflow="hidden"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box
         position="absolute"
-        top="4%"
+        top="50%"
         left="50%"
         transform="translate(-50%, -50%)"
         zIndex="1"
-        textShadow="2px 2px 2px rgba(220, 250, 229, 200)" // Agregado: sombra al texto
-        padding="1rem" // Agregado: espacio interno
-        borderRadius="8px" // Agregado: bordes redondeados
-        margin="0 auto" // Agregado: centrar el título horizontalmente
-        maxWidth="600px" // Agregado: ancho máximo del título
       >
-        Pagos Generales <br /> de la Municipalidad de Concepción
-      </Heading>
+        <Heading
+          color='black'
+          textTransform='uppercase'
+          textAlign='center'
+          fontSize='2xl'
+          fontWeight='bold'
+          textShadow="2px 2px 2px rgba(220, 250, 229, 0.5)" // Ajustado: sombra al texto
+        >
+          Pagos Generales <br /> de la Municipalidad de Concepción
+        </Heading>
       </Box>
+    </Box>
   );
 }
 
